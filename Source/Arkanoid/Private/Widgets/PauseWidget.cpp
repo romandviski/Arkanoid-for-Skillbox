61f8908609eb778426f7bad75aaa132946a9eace
Mikhail E <romandviski@mail.ru>
2025-01-25 14:45:11 +0300

    Сборка интерфейса: меню паузы с помощью C++

---
+++ Source/Arkanoid/Private/Widgets/PauseWidget.cpp
@@ -0,0 +1,57 @@
+﻿// Mikhail Efremov. All rights reserved.
+
+#include "Widgets/PauseWidget.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Kismet/GameplayStatics.h"
+
+void UPauseWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (ResumeButton)
+		ResumeButton->OnReleased.AddDynamic(this, &UPauseWidget::ResumeGame);
+	if (RestartButton)
+		RestartButton->OnReleased.AddDynamic(this, &UPauseWidget::RestartGame);
+	if (MenuButton)
+		MenuButton->OnReleased.AddDynamic(this, &UPauseWidget::BackToMenu);
+}
+
+void UPauseWidget::ResumeGame()
+{
+	OnGameResume.Broadcast();
+}
+
+void UPauseWidget::RestartGame()
+{
+	const auto LevelName = UGameplayStatics::GetCurrentLevelName(GetWorld());
+	UGameplayStatics::OpenLevel(this, FName(*LevelName));
+}
+
+void UPauseWidget::BackToMenu()
+{
+	UGameplayStatics::OpenLevel(this, FName("Menu"));
+}
+
+void UPauseWidget::SetWinStatus(const bool bWinStatus)
+{
+	if (!StatusText)
+		return;
+
+	if (ResumeButton)
+	{
+		ResumeButton->SetIsEnabled(false);
+		ResumeButton->SetVisibility(ESlateVisibility::Hidden);
+	}
+
+	if (bWinStatus)
+	{
+		StatusText->SetText(FText::FromString(TEXT("ПОБЕДА!!!")));
+		StatusText->SetColorAndOpacity(FColor::Green);
+	}
+	else
+	{
+		StatusText->SetText(FText::FromString(TEXT("ПОРАЖЕНИЕ!!!")));
+		StatusText->SetColorAndOpacity(FColor::Red);
+	}
+}
---
+++ Source/Arkanoid/Public/Widgets/PauseWidget.h
@@ -0,0 +1,45 @@
+﻿// Mikhail Efremov. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PauseWidget.generated.h"
+
+class UButton;
+class UTextBlock;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnGameResume);
+
+UCLASS()
+class ARKANOID_API UPauseWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(meta = (BindWidget, AllowPrivateAccess=true))
+	UTextBlock* StatusText = nullptr;
+	UPROPERTY(meta = (BindWidget, AllowPrivateAccess=true))
+	UButton* ResumeButton = nullptr;
+	UPROPERTY(meta = (BindWidget, AllowPrivateAccess=true))
+	UButton* RestartButton = nullptr;
+	UPROPERTY(meta = (BindWidget, AllowPrivateAccess=true))
+	UButton* MenuButton = nullptr;
+
+protected:
+	virtual void NativeConstruct() override;
+
+	UFUNCTION()
+	void ResumeGame();
+	UFUNCTION()
+	void RestartGame();
+	UFUNCTION()
+	void BackToMenu();
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "PauseWidget")
+	FOnGameResume OnGameResume;
+
+	void SetWinStatus(const bool bWinStatus);
+	
+};
