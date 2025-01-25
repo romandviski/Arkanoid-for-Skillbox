21c98764a8d854cc0d30ee9de34f69cbd1ffcd4d
Mikhail E <romandviski@mail.ru>
2025-01-25 14:39:07 +0300

    Создание класса Каретка

---
+++ Source/Arkanoid/Private/Framework/Paddle.cpp
@@ -0,0 +1,60 @@
+﻿// Mikhail Efremov. All rights reserved.
+
+
+#include "Framework/Paddle.h"
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+
+
+APaddle::APaddle()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collider"));
+	BoxCollider->SetBoxExtent(FVector(25.0f, 50.0f, 25.0f));
+	BoxCollider->SetCollisionResponseToAllChannels(ECR_Block);
+	SetRootComponent(BoxCollider);
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	StaticMesh->SetupAttachment(BoxCollider);
+
+	LeftStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Left Static Mesh"));
+	LeftStaticMesh->SetupAttachment(StaticMesh);
+	LeftStaticMesh->AddRelativeLocation(FVector(0.0f, -50.0f, 0.0f));
+	LeftStaticMesh->SetAbsolute(false, false, true);
+
+	RightStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Right Static Mesh"));
+	RightStaticMesh->SetupAttachment(StaticMesh);
+	RightStaticMesh->AddRelativeLocation(FVector(0.0f, 50.0f, 0.0f));
+	RightStaticMesh->SetAbsolute(false, false, true);
+
+	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	Arrow->SetupAttachment(StaticMesh);
+	Arrow->AddRelativeLocation(FVector(150.0f, 0.0f, 0.0f));
+	Arrow->SetAbsolute(false, false, true);
+}
+
+void APaddle::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	SetActorScale3D(DefaultScale);
+	BoxCollider->SetBoxExtent(FVector(25.0f, 50.0f + 20.0f / DefaultScale.Y, 25.0f));
+	const FVector TempScale = FVector(GetActorScale().X, GetActorScale().X, GetActorScale().Z);
+	LeftStaticMesh->SetWorldScale3D(TempScale);
+	RightStaticMesh->SetWorldScale3D(TempScale);
+}
+
+void APaddle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called to bind functionality to input
+void APaddle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Arkanoid/Public/Framework/Paddle.h
@@ -0,0 +1,51 @@
+﻿// Mikhail Efremov. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Paddle.generated.h"
+
+class ABall;
+class UArrowComponent;
+class UBoxComponent;
+
+UCLASS()
+class ARKANOID_API APaddle : public APawn
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Components")
+	UBoxComponent* BoxCollider = nullptr;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Components")
+	UStaticMeshComponent* StaticMesh = nullptr;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Components")
+	UStaticMeshComponent* LeftStaticMesh = nullptr;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Components")
+	UStaticMeshComponent* RightStaticMesh = nullptr;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true), Category="Components")
+	UArrowComponent* Arrow = nullptr;
+
+	UPROPERTY()
+	ABall* CurrentBall = nullptr;
+	
+public:
+	APaddle();
+
+protected:
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void BeginPlay() override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Settings | Game", meta=(tooltip="Класс мяча для спавна"))
+	TSubclassOf<ABall> BallClass = nullptr;
+	UPROPERTY(EditDefaultsOnly, Category = "Settings | Game")
+	int32 Lives = 3;
+	UPROPERTY(EditDefaultsOnly, Category = "Settings | Game")
+	FVector DefaultScale = FVector(0.4f, 2.4f, 0.5);
+	UPROPERTY(EditDefaultsOnly, Category = "Settings | Game")
+	float Speed = 2000.0f;
+};
